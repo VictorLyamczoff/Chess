0844fafd5a21ec3df36d48ddabde99f73066d205
dexp_son <mister.lyamczoff@yandex.ru>
2019-04-14 14:06:47 +0700

    Input Validation

---
+++ src/board_read.c
@@ -0,0 +1,13 @@
+#include "board.h"
+#include "board_print.h"
+#include "board_print_plain.h"
+#include "input.h"
+#include <stdio.h>
+#include <stdlib.h>
+void input(char move[5], int moveint[4])
+{
+    moveint[0] = move[0] - 'a' + 1;
+    moveint[1] = move[1] - '0';
+    moveint[2] = move[2] - 'a' + 1;
+    moveint[3] = move[3] - '0';
+}
